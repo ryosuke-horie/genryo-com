a2018264e07f3dc0cf28adbd6df77f7e231e0b91
ryosuke-horie <ryosuke03140@gmail.com>
2022-11-13 23:09:19 +0900

    ログイン後に体重管理のインデックスページに遷移するように修正

--- web/app/Providers/RouteServiceProvider.php
+++ web/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/weight/index';
     public const OWNER_HOME = '/owner/dashboard';
     public const ADMIN_HOME = '/admin/dashboard';
 
