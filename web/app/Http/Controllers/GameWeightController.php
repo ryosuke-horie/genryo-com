93f31f8ff8c95d4f8b3ec52c39c894597c6bc135
ryosuke-horie <ryosuke03140@gmail.com>
2022-11-17 23:34:00 +0900

    目標体重関連のMVC構築

---
+++ web/app/Http/Controllers/GameWeightController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GameWeight;
+use Illuminate\Http\Request;
+
+class GameWeightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GameWeight  $gameWeight
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GameWeight $gameWeight)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\GameWeight  $gameWeight
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(GameWeight $gameWeight)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GameWeight  $gameWeight
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GameWeight $gameWeight)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GameWeight  $gameWeight
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GameWeight $gameWeight)
+    {
+        //
+    }
+}
