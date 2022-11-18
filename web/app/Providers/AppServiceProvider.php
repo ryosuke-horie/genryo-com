3bbf92bfb409b986fabac857157851bfff91ba0a
ryosuke-horie <ryosuke03140@gmail.com>
2022-11-18 23:25:39 +0900

    サービスプロバイダに登録

--- web/app/Providers/AppServiceProvider.php
+++ web/app/Providers/AppServiceProvider.php
@@ -13,7 +13,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            // 体重
+            \App\Repositories\WeightRepositoryInterface::class,
+            \App\Repositories\WeightRepository::class,
+            // 試合体重
+            \App\Repositories\GameWeightRepositoryInterface::class,
+            \App\Repositories\GameWeightRepository::class,
+        );
+
+        $this->app->bind(
+            // 体重
+            \App\Services\WeightServiceInterface::class,
+            function ($app) {
+                return new \App\Services\WeightService(
+                    $app->make(\App\Repositories\WeightRepositoryInterface::class)
+                );
+            },
+            // 試合体重
+            \App\Services\GameWeightServiceInterface::class,
+            function ($app) {
+                return new \App\Services\GameWeightService(
+                    $app->make(\App\Repositories\GameWeightRepositoryInterface::class)
+                );
+            },
+        );
     }
 
     /**
