a15b7573f8de869cdc1a96bf3d450c627a165c4c
ryosuke-horie <ryosuke03140@gmail.com>
2022-11-30 00:09:05 +0900

    リスト表示のデフォルトレイアウトの作成

--- web/resources/views/weight/detail.blade.php
+++ web/resources/views/weight/detail.blade.php
@@ -3,7 +3,22 @@
 @include('./layouts/header')
 
 @section('content')
-test
+    <table class="table-auto m-auto">
+        <thead>
+            <tr>
+                <th class="px-4 py-2">記録日時</th>
+                <th class="px-4 py-2">体重</th>
+                <th class="px-4 py-2"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="border px-4 py-2">2022年11月28日</td>
+                <td class="border px-4 py-2">61.5</td>
+                <td class="border px-4 py-2">編集</td>
+            </tr>
+        </tbody>
+    </table>
 @endsection
 
 @include('./layouts/footer')
