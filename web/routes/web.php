ed2a226f81ddbc00c6eeb50f3fcb3a903fae555a
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-03 16:03:23 +0900

    ルーティングをグループ化

--- web/routes/web.php
+++ web/routes/web.php
@@ -15,23 +15,27 @@ Route::get('/dashboard', function () {
 /**
  * 体重遷移
  */
-// 初期ページ
-Route::get('/weight', [WeightController::class, 'index'])->middleware(['auth:users']);
-// 詳細ページ
-Route::get('/weight/detail', [WeightController::class, 'detail'])->middleware(['auth:users']);
-// 編集ページ
-Route::get('/weight/edit', [WeightController::class, 'edit'])->middleware(['auth:users']);
-// 体重入力記録機能
-Route::post('/weight/store', [WeightController::class, 'store'])->middleware(['auth:users']);
-// 体重入力修正機能
-Route::post('/weight/update', [WeightController::class, 'update'])->middleware(['auth:users']);
+Route::middleware(['auth:users'])->group(function () {
+    // 初期ページ
+    Route::get('/weight', [WeightController::class, 'index']);
+    // 詳細ページ
+    Route::get('/weight/detail', [WeightController::class, 'detail']);
+    // 編集ページ
+    Route::get('/weight/edit', [WeightController::class, 'edit']);
+    // 体重入力記録機能
+    Route::post('/weight/store', [WeightController::class, 'store']);
+    // 体重入力修正機能
+    Route::post('/weight/update', [WeightController::class, 'update']);
+});
 
 /**
  * 試合体重設定
  */
-// 試合体重設定ページ
-Route::get('/gameWeight', [GameWeightController::class, 'index'])->middleware(['auth:users']);
-// 試合体重設定機能
-Route::post('/gameWeight/store', [GameWeightController::class, 'store'])->middleware(['auth:users']);
+Route::middleware(['auth:users'])->group(function () {
+    // 試合体重設定ページ
+    Route::get('/gameWeight', [GameWeightController::class, 'index'])->middleware(['auth:users']);
+    // 試合体重設定機能
+    Route::post('/gameWeight/store', [GameWeightController::class, 'store'])->middleware(['auth:users']);
+});
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
