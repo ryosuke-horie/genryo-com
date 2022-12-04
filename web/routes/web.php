0d6eec8a2b459b980fb3261652a60157fe434466
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-04 20:32:22 +0900

    ルーティング修正

--- web/routes/web.php
+++ web/routes/web.php
@@ -12,30 +12,36 @@ Route::get('/dashboard', function () {
     return view('user.dashboard');
 })->middleware(['auth:users'])->name('dashboard');
 
-/**
- * 体重遷移
- */
 Route::middleware(['auth:users'])->group(function () {
-    // 初期ページ
-    Route::get('/weight', [WeightController::class, 'index']);
-    // 詳細ページ
-    Route::get('/weight/detail', [WeightController::class, 'detail']);
-    // 編集ページ
-    Route::get('/weight/edit', [WeightController::class, 'edit']);
-    // 体重入力記録機能
-    Route::post('/weight/store', [WeightController::class, 'store']);
-    // 体重入力修正機能
-    Route::post('/weight/update', [WeightController::class, 'update']);
-});
+    /**
+     * 体重遷移
+     */
+    Route::prefix('weight')->group(
+        function () {
+            // 初期ページ
+            Route::get('/', [WeightController::class, 'index']);
+            // 詳細ページ
+            Route::get('/detail', [WeightController::class, 'detail']);
+            // 編集ページ
+            Route::get('/edit', [WeightController::class, 'edit']);
+            // 体重入力記録機能
+            Route::post('/store', [WeightController::class, 'store']);
+            // 体重入力修正機能
+            Route::post('/update', [WeightController::class, 'update']);
+        }
+    );
 
-/**
- * 試合体重設定
- */
-Route::middleware(['auth:users'])->group(function () {
-    // 試合体重設定ページ
-    Route::get('/gameWeight', [GameWeightController::class, 'index'])->middleware(['auth:users']);
-    // 試合体重設定機能
-    Route::post('/gameWeight/store', [GameWeightController::class, 'store'])->middleware(['auth:users']);
+    /**
+     * 試合体重設定
+     */
+    Route::prefix('gameWeight')->group(
+        function () {
+            // 試合体重設定ページ
+            Route::get('/', [GameWeightController::class, 'index']);
+            // 試合体重設定機能
+            Route::post('/store', [GameWeightController::class, 'store']);
+        }
+    );
 });
 
 require __DIR__ . '/auth.php';
