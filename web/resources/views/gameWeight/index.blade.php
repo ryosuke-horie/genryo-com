2e278e37ee12d435ed9ace29304fd3a5d4137acc
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-08 19:18:22 +0900

    文言修正

--- web/resources/views/gameWeight/index.blade.php
+++ web/resources/views/gameWeight/index.blade.php
@@ -7,7 +7,7 @@
     <div class="container px-5 py-24 mx-auto">
       <div class="flex flex-col text-center w-full mb-12">
         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">出場体重設定</h1>
-        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">次回の試合で出場する体重と計量日を設定しましょう。</p>
+        <p class="lg:w-2/3 mx-auto leading-relaxed text-base">試合で出場する体重と計量日を設定しましょう</p>
       </div>
       <div class="lg:w-1/2 md:w-2/3 mx-auto">
         <form action="/gameWeight/store" method="POST">
