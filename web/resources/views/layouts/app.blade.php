dc25f9f0e44c680ca9a3e7b332245a23f0a25154
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-11 14:12:31 +0900

    Revert "タイトル修正"
    This reverts commit c257626cb7ab154d171ae0726a9edf9ad45e17e9.

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', '減量App') }}</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -18,6 +18,13 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
+            {{-- @if(auth('admin')->user())
+                @include('layouts.admin-navigation')    
+            @elseif(auth('owner')->user())
+                @include('layouts.owner-navigation')
+            @else
+                @include('layouts.user-navigation')
+            @endif --}}
             @include('layouts.user-navigation')
             <!-- Page Heading -->
             <header class="bg-white shadow">
