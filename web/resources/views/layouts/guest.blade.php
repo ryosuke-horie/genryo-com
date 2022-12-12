6293e7d4fa35fdc69e4fbb7b550e942afbd604ca
ryosuke-horie <ryosuke03140@gmail.com>
2022-12-12 23:22:48 +0900

    https対応

--- web/resources/views/layouts/guest.blade.php
+++ web/resources/views/layouts/guest.blade.php
@@ -1,24 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', '減量App') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title>{{ config('app.name', '減量App') }}</title>
 
-        <!-- Styles -->
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    @if (config('app.env') === 'production')
         <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+    @else
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @endif
 
-        <!-- Scripts -->
+    <!-- Scripts -->
+    @if (config('app.env') === 'production')
         <script src="{{ secure_asset('js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
-    </body>
+    @else
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    @endif
+</head>
+
+<body>
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
+</body>
+
 </html>
